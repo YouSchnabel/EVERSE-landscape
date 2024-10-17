008e0ccbe3ef73666b3a0986a21d9658837df1ae
Jutta Schnabel <jutta.schnabel@fau.de>
2024-10-17 21:39:20 +0200

    first survey report

--- README.md
+++ README.md
@@ -2,16 +2,33 @@
 
 This repository serves to evaluate the outcomes of the landscaping survey from WP2 of the EVERSE project.
 
-You can find the outcome of the survey in the *docs*
+You can find the outcome of the survey in the *data* folder and generate a documentation of the survey outcomes by using the notebooks provided in *notebooks*.
 
-## About the survey
+## Contents
+* **data** contains the survey results, data annotation and steering file to produce a report
+* **src** contains supplementary functions
+* **notebooks** contains the Jupyter notebooks to evaluate the survey
 
 ## Getting started
 
-## Contents
+All survey responses are read and assigned an ID. Reading of the data is handled in the *DataSet* class, creating a plot with the *ReportMaker* class.
 
-* **data** contains the survey results, data annotation and encoding
-* **src** contains supplementary functions
-* **notebooks** contains the Jupyter notebooks to evaluate the survey
-* **docs** generates the project documentation and survey results
+### Creating a report
 
+See the notebook "Overview.ipynb" for an overview report of the survey. The structure of the survey is defined in the /data/structure.yml file. Here, each "cluster" of questions is displayed as a table, and a title and description for each chapter and table can be allocated.
+
+## Open Questions
+
+### Planned additional functionalities
+
+- [ ] Adding display options for ratings and selection questions
+- [ ] Weighting according to confidence questions
+- [ ] Filtering of the dataset according to selection options (e.g. science cluster, representative view, ...)
+- [ ] Documentation on use
+
+### Display options
+
+How should the result of the survey be displayed? Git pages? Pdf? ...?
+--> Proposal to make the outcomes available as e.g. webpage and reference full report in the deliverable, using only highlight excerpts for report.
+
+Any other additional requests?
--- src/surveyreader.py
+++ src/surveyreader.py
@@ -3,12 +3,17 @@
 import pandas as pd
 import json
 
+mapperfile = "../data/survey_transform.json"
+
+idmapping = {}
+
 class DataSet:
     def __init__(self, filepath):
         self.metadata = {}
         self.data = pd.DataFrame()
 
         self._read_xls(filepath)
+        self._preprocess()
 
     def _read_xls(self, filepath):
         #Reading the survey results from the export from EU survey
@@ -16,49 +21,116 @@ class DataSet:
         
     def _preprocess(self):
         # Extracting metadata from the survey file
+        
+        # First, identify all questions and relations
         questions = {}
         
         for colname in self.data.columns:
+            nicename = colname.replace("applicable:", "applicable ")
+            subtexts = nicename.split(":")
+            questionid = False
+            for qid in questions:
+                if questions[qid]["question"] == subtexts[0]:
+                    questionid = qid
+            if not questionid:
+                if len(subtexts) == 1:
+                    subtexts[0] = colname
+                questions.setdefault("A"+str(len(questions)), {
+                    "question": subtexts[0], 
+                    "subtexts": [], 
+                    "colnames": [], 
+                    "entrytype": "undefined",
+                    "params": {}
+                })
+                qid = "A"+str(len(questions)-1)
+            if len(subtexts)>1:
+                questions[qid]["subtexts"].append(subtexts[1:len(subtexts)])
+                questions[qid]["colnames"].append(colname)
+        self.metadata = questions
+
+        # Then, preprocess
+        for qid in self.metadata:
+            if len(self.metadata[qid]["subtexts"]) == 0:
+                uniqueanswers = self.data[self.metadata[qid]["question"]].unique()
+                answers = [x for x in uniqueanswers if pd.notna(x)]
+
+                # Try to decide which entry type it is
+                if len(answers) == 0:
+                    self.metadata[qid]["entrytype"] = "no answers"
+                elif type(answers[0]) is str:
+                    self.metadata[qid]["entrytype"] = "textlist"
+
+            else:
+                uniqueanswers = self.data[self.metadata[qid]["colnames"][0]].unique()
+                answers = [x for x in uniqueanswers if pd.notna(x)]
+                if len(answers) == 0:
+                    self.metadata[qid]["entrytype"] = "no answers"
+                elif str(answers[0]).find("/5")>0 or str(answers[0]).find("/10")>0:
+                    self.metadata[qid]["entrytype"] = "rating"
+
             # Get the ID of the question from the header
+            '''
             colid = colname[colname.rfind("(")+1:-1]
             questionid = colid.split("_")[0]
 
-            subtexts = colname.split(" : ")
+            subtexts = colname.split(":")
             if len(subtexts)>2:
-                subtexts = subtexts[-3:-1]
+                subtexts = subtexts[1][-3:-1]
+                if not subtexts[0] in idmapping:
+                    idmapping.setdefault(subtexts[0], "A"+str(len(idmapping)))
+                questionid = idmapping[subtexts[0]]
             else:
                 subtexts = []
+            '''
+            # figure out which entry type it is
+            
+
+            # cluster
             
             # Get all answer options
+        '''
             uniqueanswers = self.data[colname].unique()
             answers = [x for x in uniqueanswers if pd.notna(x)]
 
             # Try to decide which entry type it is
             if len(answers) == 0:
                 entrytype = "no answers"
-            elif answers[0].find(")")>0:
+            elif str(answers[0]).find(")")>0:
                 entrytype = "select"
-            elif len(answers[0])<6 and answers[0].find("/")>0:
+            elif len(str(answers[0]))<6 and str(answers[0]).find("/")>0:
                 entrytype = "rating"
-            elif all(len(item) == answers[0] for item in answers):
+            elif all(len(str(item)) == answers[0] for item in answers):
                 entrytype = "ranking"
             else:
                 entrytype = "text" 
-            
+
             columninfo = {
                 "subids": colid.split("_"),
                 "text": colname[0:colname.rfind("(")-1],
                 "subtexts": subtexts,
                 "type": entrytype,
-                "answers": self._get_answers_metadata(self.data[colname], entrytype)
+                "answers": self._get_answers_metadata(answers, entrytype)
             }
-            
+
             questions.setdefault(questionid, [])
             questions[questionid].append(columninfo)
-        self.metadata.setdefault("questions", questions)
+        self.metadata.setdefault("questions", questions)'''
+
+    def list_questions(self):
+        # Returns list of identifiers and questions in the form ID - question (type)
+        for key in self.metadata:
+            print (key, "-", self.metadata[key]["question"], "("+self.metadata[key]["entrytype"]+")")    
 
     @staticmethod
-    def _get_answers_metadata(column, columntype):
-        # Adjusting entries or better evaluation
-        entrymeta = {}
+    def _get_answers_metadata(uniqueanswers, columntype):
+        # Adjusting entries for better evaluation
+        if len(uniqueanswers) > 10 and columntype == "text":
+            entrymeta = {"type": "individual", "enum": []}
+        elif columntype == "ranking":
+            print ("ranking:", uniqueanswers)
+        elif columntype == "rating":
+            print ("RATE:", uniqueanswers)
+            entrymeta = {"type": "rate", "total": float(uniqueanswers[0].split("/")[1])}
+        else:
+            entrymeta = {}
         return entrymeta
